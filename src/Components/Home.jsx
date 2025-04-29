d0ca8c2b45e468d67ce6395366161afe811fa703
Andre <andre_ranulfo@hotmail.com>
2025-04-29 10:50:12 -0300

    .

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-
 const Home = () => {
   return (
     <section className='container'>
       <div className="row mt-3 justify-content-center ">
         <div className="col-8 card text-center p-3 bg-light">
-
-      <h1>Home</h1>
+          <h1>Home</h1>
         </div>
-
       </div>
-      </section>
+    </section>
   )
 }
 
