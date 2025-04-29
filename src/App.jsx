a780a8dbaaffe4c9a22d00d471cec7697b1127de
Andre <andre_ranulfo@hotmail.com>
2025-04-28 23:49:57 -0300

    Router created

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import Contacts from './Components/Contacts'
 import Home from './Components/Home'
 import Services from './Components/Services'
+import Error from './Components/Error'
 
 function App() {
 
 
   return (
     <>
-   
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/services' element={<Services />} />
-      <Route path='/Contacts' element={<Contacts />} />
-    </Routes>
-    
-    </BrowserRouter>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/Contacts' element={<Contacts />} />
+          <Route path='/*' element={<Error />} />
+
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
