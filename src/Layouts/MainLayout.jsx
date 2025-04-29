796dda61761959ff6bc7759358b7c8996da90ca3
Andre <andre_ranulfo@hotmail.com>
2025-04-29 11:03:19 -0300

    style header

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -6,13 +6,13 @@ import { FaRegEnvelope } from "react-icons/fa6";
 function MainLayout() {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <header>
-            <nav className='container justify-content-center mt-3'>
+      <div className="container-fluid">
+        <div className="row ">
+          <header className='container-fluid bg-dark text-light'>
+            <nav className='container-fluid justify-content-center mt-3 '>
               <h1>My Website</h1>
-              <div className="row">
-                <div className="col text-center">
+              <div className="row ">
+                <div className="col text-center mb-3">
                   <Link className='btn btn-primary me-3 ' to="/"> <FaHome />  Home</Link>
                   <Link className='btn btn-primary me-3' to="/services"><MdCleaningServices />Services</Link>
                   <Link className='btn btn-primary' to="/contacts"><FaRegEnvelope />Contacts</Link>
@@ -22,8 +22,10 @@ function MainLayout() {
           </header>
           {/* Aqui vai ser renderizado o conteúdo de cada página */}
           <Outlet />
-          <footer>
+          <footer className='container mt-5'>
+            <div className="row">
             <p>© 2025 My Website</p>
+            </div>
           </footer>
         </div>
       </div>
