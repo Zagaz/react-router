965e60878f1d0eaf7efcdeb9bf3b7e9902d8728c
Andre <andre_ranulfo@hotmail.com>
2025-04-29 13:24:57 -0300

    useNavigate

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { useNavigate } from "react-router";
 import { FaHome } from "react-icons/fa";
 const Home = () => {
+  let navigate = useNavigate();
   return (
     <section className='container'>
       <div className="row mt-3 justify-content-center ">
         <div className="col-8 card text-center p-3 bg-light">
           <h1><FaHome /> Home</h1>
         </div>
+        <button
+        onClick={()=>{
+          navigate('/services')
+        }}
+        >
+          Services
+
+        </button>
+       
       </div>
     </section>
   )
